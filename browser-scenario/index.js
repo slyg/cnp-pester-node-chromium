9c90e56764c978143e0126041c972eca3927890a
Sylvain Faucherand <syl.faucherand@gmail.com>
2019-03-11 11:18:31 +0000

    Use Windows agent

--- browser-scenario/index.js
+++ browser-scenario/index.js
@@ -1,10 +1,14 @@
 const puppeteer = require("puppeteer-core");
 const debug = require("debug")("node:puppeteer");
-const CHROME_BIN = "/usr/bin/google-chrome";
+const CHROME_BIN =
+  "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe";
 
 (async () => {
   try {
-    const browser = await puppeteer.launch({ executablePath: CHROME_BIN });
+    const browser = await puppeteer.launch({
+      executablePath: CHROME_BIN,
+      headless: true
+    });
     const page = await browser.newPage();
     await page.goto("https://www.gov.uk/", {
       waitUntil: "domcontentloaded"
