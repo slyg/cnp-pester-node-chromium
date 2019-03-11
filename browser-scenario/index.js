a4f0c9b9054cf90765b45f08ba7b5a598e18d1bc
Sylvain Faucherand <syl.faucherand@gmail.com>
2019-03-11 12:49:13 +0000

    Do not catch browser instrumentation errors

--- browser-scenario/index.js
+++ browser-scenario/index.js
@@ -4,22 +4,18 @@ const CHROME_BIN =
   "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe";
 
 (async () => {
-  try {
-    const browser = await puppeteer.launch({
-      executablePath: CHROME_BIN,
-      headless: true
-    });
-    const page = await browser.newPage();
-    await page.goto("https://www.gov.uk/", {
-      waitUntil: "domcontentloaded"
-    });
-    const title = await page.evaluate(() =>
-      document.querySelector("h1").textContent.trim()
-    );
-    debug(`Page title is: ${title}`);
-    await browser.close();
-  } catch (e) {
-    console.error(`Wooops`);
-    debug(e);
-  }
+  const browser = await puppeteer.launch({
+    executablePath: CHROME_BIN,
+    headless: true
+  });
+  const page = await browser.newPage();
+  await page.goto("https://www.gov.uk/", {
+    waitUntil: "domcontentloaded"
+  });
+  const title = await page.evaluate(() =>
+    document.querySelector("h1").textContent.trim()
+  );
+  debug(`Page title is: ${title}`);
+  await browser.close();
+  process.stdout.write(title);
 })();
